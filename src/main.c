816eb0092ff64f3f8bf786a3bad71f3bc9a308ad
D. Sunshine <david@sunshines.org>
2026-09-07 19:25:09 -0400

    Rule and rotation checkpoint

--- src/main.c
+++ src/main.c
@@ -3,6 +3,8 @@
 
 #include "cards.h"
 
+extern Texture2D CARD_TEXTURES[NUM_CARDS];
+
 int main(void) {
         int image_height = CARD_HEIGHT * 2;
         int image_width  = (int) ((((float) image_height) * 16.0f) / 9.0f);
@@ -21,13 +23,66 @@ int main(void) {
                 .scale = 1.0f,
         };
 
+        float h = (float) image_width/2.0f;
+        float k = (float) image_height/2.0f;
+
+        Rectangle src = {0.0f, 0.0f, (float) CARD_WIDTH, (float) CARD_HEIGHT};
+        Rectangle dst = {h, k,
+                         (float) CARD_WIDTH, (float) CARD_HEIGHT};
+        Vector2   org = {(float) CARD_WIDTH, (float) CARD_HEIGHT};
+
+        float rot = 15.0f;
+
+        float ox = h - CARD_WIDTH/2.0f;
+        float oy = k - CARD_HEIGHT/2.0f;
+
+        float xp = (ox - h) * cos(rot) - (oy - k) * sin(rot) + h;
+        float yp = (ox - h) * sin(rot) + (oy - k) * cos(rot) + k;
+        float dx = ox - xp;
+        float dy = oy - yp;
+
         while (!WindowShouldClose()) {
                 BeginDrawing();
 
                 ClearBackground((Color) {216, 222, 233, 255});
 
-                draw_card(HWATU_APRIL_TANE, style, ((float) image_width)/2.0f, ((float) image_height)/2.0f);
+                Texture2D image = CARD_TEXTURES[GET_INDEX(HWATU_APRIL_TANE)];
+                rot = 15 * 3.14 / 180.0f;
+                xp = (ox - h) * cos(rot) - (oy - k) * sin(rot) + h;
+                yp = (ox - h) * sin(rot) + (oy - k) * cos(rot) + k;
+                dx = ox - xp;
+                dy = oy - yp;
+                dst = (Rectangle) {h + CARD_WIDTH/2.0f + dx, k + CARD_HEIGHT/2.0f + dy, (float) CARD_WIDTH, (float) CARD_HEIGHT};
+                rot = 15;
+                DrawTexturePro(image, src, dst, org, rot, WHITE);
 
+                DrawLine((int)ox, 0, (int)ox, image_height, RED);
+                DrawLine(0, (int)oy, image_width, (int)oy, RED);
+                
+                DrawLine((int)xp, 0, (int)xp, image_height, BLUE);
+                DrawLine(0, (int)yp, image_width, (int)yp, BLUE);
+                
+                /* rot = 5.0f; */
+                /* xp = (ox - h) * cos(rot) - (oy - k) * sin(rot) + h; */
+                /* yp = (ox - h) * sin(rot) + (oy - k) * cos(rot) + k; */
+                /* dx = ox - xp; */
+                /* dy = oy - yp; */
+                /* dst = (Rectangle) {h + dx, k + dy, (float) CARD_WIDTH, (float) CARD_HEIGHT}; */
+                /* image = CARD_TEXTURES[GET_INDEX(HWATU_MAY_TANE)]; */
+                /* DrawTexturePro(image, src, dst, org, rot, WHITE); */
+                
+                /* rot = -15.0f; */
+                /* xp = (ox - h) * cos(rot) - (oy - k) * sin(rot) + h; */
+                /* yp = (ox - h) * sin(rot) + (oy - k) * cos(rot) + k; */
+                /* dx = ox - xp; */
+                /* dy = oy - yp; */
+                /* dst = (Rectangle) {h + dx, k + dy, (float) CARD_WIDTH, (float) CARD_HEIGHT}; */
+                /* image = CARD_TEXTURES[GET_INDEX(HWATU_JUNE_TANE)]; */
+                /* DrawTexturePro(image, src, dst, org, rot, WHITE); */
+
+                DrawLine((int)image_width/2, 0, (int)image_width/2, image_height, GRAY);
+                DrawLine(0, (int)image_height/2, image_width, (int)image_height/2, GRAY);
+                /* draw_card(HWATU_APRIL_TANE, style, ); */
                 EndDrawing();
         }
 
